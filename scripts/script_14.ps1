1f0ea902221b03c801f636f733714942761a0230
Juan Ramón Rueda <rayseink_1694@hotmail.com>
2022-11-15 13:07:41 +0100

    Se estructuran los scripts para usar variables de fuera de las funciones

--- scripts/script_14.ps1
+++ scripts/script_14.ps1
@@ -1,33 +1,42 @@
-# ----------------------------------------------------------------------------------------------------------------------------------------------------
-# Limpieza de pantalla
-Clear-Host
-
-
-# ----------------------------------------------------------------------------------------------------------------------------------------------------
-# Conversión de parámetro a variable
+#-------------------------------------------------------------------------------------------------------------------------------------------------------
+# Conversión de un argumento a variable
 $parametro1=$Args[0]
 
-# ----------------------------------------------------------------------------------------------------------------------------------------------------
-# Creación de función para:
-# Pasar por tubería (pipe) el contenido del directorio listado para agruparlo por extensión para seleccionar campos, agrupar por nombre
-# (extensión) y realizar una tabla de lo listado del directorio
-function Obtener_tabla {
+
+#-------------------------------------------------------------------------------------------------------------------------------------------------------
+# Creación de función para agrupar por extensión y mostrar en forma de tabla
+function list_windows_group_extension {
     Get-ChildItem -Path C:\Windows\System32 | Group-Object -Property Extension | Select-Object Count,Name | Sort-Object -Property Name | Format-Table
 }
 
 
-# ----------------------------------------------------------------------------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------------------------------------------------------------------------------
 # Comprobación si hay parámetro dado al script, si recibe un parámetro, creará un .txt con el nombre del parámetro dado
-if ($null -eq $parametro1) {
-    Obtener_tabla
-} else {
-    if (Test-Path C:\pruebas_scripts) {
-        Write-Host 'Directorio creado anteriormente'
-    }
-    else {
-        New-Item -Path C:\pruebas_scripts -ItemType Directory
-        Write-Host 'Directorio creado'
-    }
+function main {
+    `$parametro1 = $parametro1
 
-    Obtener_tabla > C:\pruebas_scripts\$parametro1.txt
+    Invoke-Expression .\clear_display.ps1
+
+    if ($null -eq $parametro1) {
+        list_windows_group_extension
+    }
+    
+    else {
+        if (Test-Path C:\pruebas_scripts) {
+            Write-Host 'Directorio creado anteriormente'
+        }
+
+        else {
+            New-Item -Path C:\pruebas_scripts -ItemType Directory
+            Write-Host 'Directorio creado'
+        }
+    
+        list_windows_group_extension > C:\pruebas_scripts\$parametro1.txt
+    }
+    
+}
+
+#-------------------------------------------------------------------------------------------------------------------------------------------------------
+if ($MyInvocation.InvocationName -ne '&') {
+    main
 }
--- scripts/script_15.ps1
+++ scripts/script_15.ps1
@@ -1,29 +1,40 @@
 #-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-# Definición de variables para el argumento
+# Conversión de un argumento a variable
 $parametro1=$Args[0]
 
 
 #-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 # Declaración de función para listar los directorios y subdirectorios de más de 2MB y último acceso en los últimos 7 días
-function tamaño_acceso_directorios {
+function min_length_max_lastAccessTime {
     Get-ChildItem -Path C:\Windows -Recurse -Force | Where-Object {($_.Length -gt 2MB) -and ($_.LastAccessTime -lt (Get-Date).AddDays(-7))} | Select-Object Name | Format-Wide -Column 3
 }
 
 #-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 # Comprobación de que el script recibe o no parámetros
-if ($null -eq $parametro1) {
-    
-    tamaño_acceso_directorios
+function main {
+    if ($null -eq $parametro1) {
 
-} else {
+        `$parametro1 = $parametro1
+        
+        min_length_max_lastAccessTime
     
-    if (Test-Path C:\pruebas_scripts) {
-        Write-Host 'Directorio creado anteriormente'
+    } else {
+        
+        if (Test-Path C:\pruebas_scripts) {
+            Write-Host 'Directorio creado anteriormente'
+        }
+        else {
+            New-Item -Path C:\pruebas_scripts -ItemType Directory
+            Write-Host 'Directorio creado'
+        }
+    
+        min_length_max_lastAccessTime > C:\pruebas_scripts\$parametro1.txt
     }
-    else {
-        New-Item -Path C:\pruebas_scripts -ItemType Directory
-        Write-Host 'Directorio creado'
-    }
+}
 
-    tamaño_acceso_directorios > C:\pruebas_scripts\$parametro1.txt
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# Ejecución de la función principal "main"
+if ($MyInvocation.InvocationName -ne '&') {
+    main
 }
