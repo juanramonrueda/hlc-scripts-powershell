85391dbefb82fa5c61ece697c71196a9af01debf
Juan Ramón Rueda <rayseink_1694@hotmail.com>
2022-11-15 12:09:07 +0100

    Se actualiza el script
    Se actualiza para mostrar únicamente los comandos, se usan funciones para almacenar cada comando usado y se hace un filtrado de los procesos más minucioso

--- scripts/script_10.ps1
+++ scripts/script_10.ps1
@@ -1,17 +1,39 @@
-# ------------------------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------------
 # Limpieza de pantalla
-Clear-Host
-
-# ------------------------------------------------------------------------------------------------
-# Creación de un directorio para almacenar los archivos relacionados con los scripts si no existe
-if (Test-Path C:\pruebas_scripts) {
-    Write-Host 'Directorio creado anteriormente'
-}
-else {
-    New-Item -Path C:\pruebas_scripts -ItemType Directory
-    Write-Host 'Directorio creado'
+function clear_display {
+    Clear-Host
 }
 
-# ------------------------------------------------------------------------------------------------
+
+#-----------------------------------------------------------------------------------------------------------
 # Ejecución del comando para guardar en un fichero los comandos que empiezen por Process
-Get-Command -Name *Process* > C:\pruebas_scripts\fichero_script_10.txt
+function get_command_process {
+    Get-Command -Name *-Process* | Select-Object -Property Name > C:\pruebas_scripts\fichero_script_10.txt
+}
+
+
+#-----------------------------------------------------------------------------------------------------------
+# Creación de un directorio para almacenar los archivos relacionados con los scripts si no existe
+function main {
+    clear_display
+
+    if (Test-Path C:\pruebas_scripts) {
+        Write-Host 'Directorio creado anteriormente'
+
+        get_command_process
+    }
+
+    else {
+        New-Item -Path C:\pruebas_scripts -ItemType Directory
+        Write-Host 'Directorio creado'
+
+        get_command_process
+    }
+}
+
+
+#-----------------------------------------------------------------------------------------------------------
+# Ejecución de la función principal "main"
+if ($MyInvocation.InvocationName -ne '&') {
+    main
+}
