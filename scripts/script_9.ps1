b89429d904e97c3c81c37c33b754094d7a25723d
Juan Ramón Rueda <rayseink_1694@hotmail.com>
2022-11-15 12:00:05 +0100

    Se actualiza el script para mostrar únicamente los comandos

--- scripts/script_9.ps1
+++ scripts/script_9.ps1
@@ -1,19 +1,40 @@
-# ------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------------
 # Limpieza de pantalla
-Clear-Host
-
-# ------------------------------------------------------------------------------------------------
-# Creación de un directorio para almacenar los archivos relacionados con los scripts si no existe
-if (Test-Path C:\pruebas_scripts) {
-    Write-Host 'Directorio creado anteriormente'
-}
-else {
-    New-Item -Path C:\pruebas_scripts -ItemType Directory
-    Write-Host 'Directorio creado'
+function clear_display {
+    Clear-Host
 }
 
 
-# ------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------------
 # Ejecución del comanado para guardar en un fichero todos los comandos que tengan el verbo set
+function get_commands_set {
+    Get-Command -verb set | Select-Object -Property Name > C:\pruebas_scripts\fichero_script_9.txt
+}
 
-Get-Command -verb set > C:\pruebas_scripts\fichero_script_9.txt
+
+#--------------------------------------------------------------------------------------------------------
+# Función para crear un directorio para almacenar los archivos relacionados con los scripts si no existe
+function main {
+    clear_display
+
+    if (Test-Path C:\pruebas_scripts) {
+        Write-Host 'Directorio creado anteriormente'
+        
+        get_commands_set
+    }
+    
+    else {
+        New-Item -Path C:\pruebas_scripts -ItemType Directory
+        Write-Host 'Directorio creado'
+    
+        get_commands_set
+    }
+    
+}
+
+
+#--------------------------------------------------------------------------------------------------------
+# Ejecución de la función principal "main"
+If ($MyInvocation.InvocationName -ne '&') {
+    main
+}
