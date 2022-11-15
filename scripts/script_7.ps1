862807979d276997359cc2534b82f9c3ba37fcbd
Juan Ramón Rueda <rayseink_1694@hotmail.com>
2022-11-15 16:12:39 +0100

    Se usan funciones para la estructuración de los scripts

--- scripts/script_7.ps1
+++ scripts/script_7.ps1
@@ -1,14 +1,29 @@
-# -------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------------------
 # Limpieza de pantalla
-Clear-Host
+Invoke-Expression .\clear_display.ps1
 
-# -------------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------------------------
 # Petición de un número por pantalla
-$numero = Read-Host 'Introduzca un numero para obtener su tabla de recorridopicar'
+function aks_for_number {
+    $script:numero = Read-Host 'Introduzca un numero para obtener su tabla de multiplicar'
+}
 
-# -------------------------------------------------------------------------------
-# Realización de la recorridoplicación y salida vistosa de la tabla de recorridoplicar
-for ($recorrido = 1; $recorrido -le 10; $recorrido++) {
-    $resultado = $recorrido * $numero
-    Write-Host $recorrido 'x' $numero '=' $resultado
+
+#-------------------------------------------------------------------------------------------
+# Realización de la multiplicación y salida vistosa de la tabla de multiplicar
+function main_s_7 {
+    aks_for_number
+
+    for ($recorrido = 1; $recorrido -le 10; $recorrido++) {
+        $resultado = $recorrido * $numero
+        Write-Host $recorrido 'x' $numero '=' $resultado
+    }
+}
+
+
+#-------------------------------------------------------------------------------------------
+# Ejecución de la función principal "main"
+if ($MyInvocation.InvocationName -ne '&') {
+    main_s_7
 }
--- scripts/script_8.ps1
+++ scripts/script_8.ps1
@@ -1,30 +1,44 @@
-# -------------------------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------------------------------
 # Limpieza de pantalla
-Clear-Host
+Invoke-Expression .\clear_display.ps1
 
-# -------------------------------------------------------------------------------------------------------------------
-# Declaración de variables necesarias
-$contador_posicion = 0
 
-# -------------------------------------------------------------------------------------------------------------------
-# Petición de una palabra
-$palabra = Read-Host 'Introduzca una palabra para saber cual es la primera vocal y su posicion'
+#--------------------------------------------------------------------------------------------------------------------------
+# Declaración de variable para iniciar el contador de posición a 0
+$script:contador_posicion = 0
 
-# -------------------------------------------------------------------------------------------------------------------
-# Compruebo cuantas letras tiene la palabra anterior y guardo la cantidad en una variable
-$cantidad = $palabra.Length
 
-# -------------------------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------------------------------
+# Petición de una palabra y conteo de letras que tiene
+function ask_for_word {
+    $script:palabra = Read-Host 'Introduzca una palabra para saber cual es la primera vocal y su posicion'
+
+    $script:cantidad = $palabra.Length
+}
+
+
+
+#--------------------------------------------------------------------------------------------------------------------------
 # Se recorre cada letra de la palabra introducida mediante un for y se usa substring para saber dónde está la vocal
-for ($inicio = 0; $inicio -le $cantidad; $inicio++) {
+function main_s_8 {
+    ask_for_word
+
+    for ($inicio = 0; $inicio -le $cantidad; $inicio++) {
     
-    $contador_posicion = $contador_posicion + 1
-    
-    $letra = $palabra.Substring($inicio,1)
-    
-    if (($letra -eq 'a') -or ($letra -eq 'e') -or ($letra -eq 'i') -or ($letra -eq 'o') -or ($letra -eq 'u')) {
-        Write-Host 'La primera vocal de la palabra' $palabra 'es' $letra 'y su posicion es' $contador_posicion
-        break
+        $contador_posicion = $contador_posicion + 1
+        
+        $letra = $palabra.Substring($inicio,1)
+        
+        if (($letra -eq 'a') -or ($letra -eq 'e') -or ($letra -eq 'i') -or ($letra -eq 'o') -or ($letra -eq 'u')) {
+            Write-Host 'La primera vocal de la palabra' $palabra 'es la' $letra 'y su posicion es la' $contador_posicion
+            break
+        }
     }
-    
+}
+
+
+#--------------------------------------------------------------------------------------------------------------------------
+# Ejecución de la función principal "main"
+if ($MyInvocation.InvocationName -ne '&') {
+    main_s_8
 }
