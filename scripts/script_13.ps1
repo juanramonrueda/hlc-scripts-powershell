a93072735f605378413c5921a769bacf394261e3
Juan Ramón Rueda <rayseink_1694@hotmail.com>
2022-11-17 13:28:36 +0100

    Se usa variable para el path de los ejecutables

--- scripts/script_13.ps1
+++ scripts/script_13.ps1
@@ -1,9 +1,13 @@
 #--------------------------------------------------------------------------------------------------------------
+# Definición de variables
+$script:execute_files = C:\Windows\System32\*
+
+#--------------------------------------------------------------------------------------------------------------
 # Se muestra por pantalla los ejecutables, tanto *.exe como *.com de C:\Windows\System32 junto a sus atributos
 function main_s_13 {
     Invoke-Expression .\clear_display.ps1
 
-    Get-ChildItem -Path C:\Windows\System32\* -Include ('*.exe', '*.com') | Select-Object Name,Mode,Length
+    Get-ChildItem -Path $execute_files -Include ('*.exe', '*.com') | Select-Object Name,Mode,Length
 }
 
 
