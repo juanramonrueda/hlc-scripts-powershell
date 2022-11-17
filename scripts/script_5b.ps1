fb9e5fa2e8b6b301a30ef4ae98c04870a96b6245
Juan Ramón Rueda <rayseink_1694@hotmail.com>
2022-11-17 08:25:15 +0100

    Se cambia la comparación para controlar el tipo de dato introducido

--- scripts/script_5b.ps1
+++ scripts/script_5b.ps1
@@ -1,16 +1,16 @@
-#------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------
 # Limpieza de pantalla
 Invoke-Expression .\clear_display.ps1
 
 
-#------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------
 # Definición de los argumentos a variables
 $script:variable_1 = $Args[0]
 
 $script:variable_2 = $Args[1]
 
 
-#------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------
 # Comparación de los argumentos pasados para comparar qué número es mayor
 function main_s_5b {
     if ($variable_1 -gt $variable_2) {
@@ -21,14 +21,18 @@ function main_s_5b {
         $resultado = 'El numero ' + $variable_2 + ' es mayor que el numero ' + $variable_1
     }
     
-    else {
+    elseif ($variable_1 -eq $variable_2) {
         $resultado = 'Los numeros son iguales'
+    }
+
+    else {
+        $resultado = 'Ha introducido texto, por lo que no se puede hacer la comparación'
     }
 
     return $resultado
 }
 
-#------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------
 # Ejecución de la función principal "main"
 if ($MyInvocation.InvocationName -ne '&') {
     main_s_5b
