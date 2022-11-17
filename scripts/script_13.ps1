72e992bba7990246097a6d6a2d1d56e012016e2d
Juan Ramón Rueda <rayseink_1694@hotmail.com>
2022-11-17 13:30:21 +0100

    Se usa variable para la extensión de los ejecutables

--- scripts/script_13.ps1
+++ scripts/script_13.ps1
@@ -2,12 +2,15 @@
 # Definición de variables
 $script:execute_files = C:\Windows\System32\*
 
+$script:files_extension = '*.exe', '*.com'
+
+
 #--------------------------------------------------------------------------------------------------------------
 # Se muestra por pantalla los ejecutables, tanto *.exe como *.com de C:\Windows\System32 junto a sus atributos
 function main_s_13 {
     Invoke-Expression .\clear_display.ps1
 
-    Get-ChildItem -Path $execute_files -Include ('*.exe', '*.com') | Select-Object Name,Mode,Length
+    Get-ChildItem -Path $execute_files -Include ($files_extension) | Select-Object Name,Mode,Length
 }
 
 
