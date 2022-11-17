ce4b6d3f8c0f79ffe912136aabd36069dd6cdbc9
Juan Ramón Rueda <rayseink_1694@hotmail.com>
2022-11-17 13:08:28 +0100

    Se reestructuran las funciones del script

--- scripts/script_12.ps1
+++ scripts/script_12.ps1
@@ -7,16 +7,7 @@ $script:file = "fichero_script_12.txt"
 
 #-------------------------------------------------------------------------------------------------
 # Se obtienen los nombres de los ficheros .txt que hay en la raíz del sistema operativo
-function list_disk_c {
-    Get-ChildItem -Name -Path C:\*.txt >  $dir_path\$file
-}
-
-
-#-------------------------------------------------------------------------------------------------
-# Creación de un directorio para almacenar los archivos relacionados con los scripts si no existe
-function main_s_12 {
-    Invoke-Expression .\clear_display.ps1
-
+function create_directory {
     if (Test-Path $dir_path) {
         Write-Host 'El directorio' $dir_path "esta creado"
     }
@@ -25,8 +16,15 @@ function main_s_12 {
         New-Item -Path $dir_path -ItemType Directory
         Write-Host 'Se ha creado el directorio' $dir_path
     }
+}
 
-    list_disk_c
+
+#-------------------------------------------------------------------------------------------------
+# Creación de un directorio para almacenar los archivos relacionados con los scripts si no existe
+function main_s_12 {
+    create_directory
+
+    Get-ChildItem -Name -Path C:\*.txt >  $dir_path\$file
 }
 
 
